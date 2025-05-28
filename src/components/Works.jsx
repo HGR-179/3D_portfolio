d6bbdf135c94ba427439a9bba2dd866d1ee89968
HGR-179 <abraham.y0317@gmail.com>
2025-05-28 20:59:31 +0900

    master

---
+++ src/components/Works.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { LinkedIn } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt option={{max: 45, scale: 1, speed: 450,}} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[230px]">
+          <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl"/>
+          
+          <div className="absolute inset-0 flex justify-end m-3 card-image_hover">
+            <div onClick={() => window.open(source_code_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              <img src={LinkedIn} alt="LinkedIn" className="w-1/2 h-1/2 object-contain" />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
+
+const Works = () => {
+  return (
+    <>
+       <motion.div variants={textVariant()}>
+        <h2 className={`${styles.sectionHeadText}`}>About Me and My School.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          Feel free to explore my LinkedIn profile and my school's official page by clicking on the logos below. 
+          You'll find detailed information about my skills, experiences, and the excellent education I am receiving.
+          Whether you want to learn more about my professional journey or the institution that shapes my growth,
+          just click the icons to dive in!
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Works, "");
---
+++ src/constants/index.js
@@ -0,0 +1,262 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  linkedin_home,
+  School_LinkedIn,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "CSS Styling Explorer",
+    company_name: "css",
+    icon: css,
+    iconBg: "#383E56",
+    date: "March 2023 - April 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "JavaScript Enthusiast",
+    company_name: "Javascript",
+    icon: javascript,
+    iconBg: "#E6DEDD",
+    date: "March 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Builder in Training",
+    company_name: "Shopify",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "ReactJS Learner",
+    company_name: "Reactjs",
+    icon: reactjs,
+    iconBg: "#E6DEDD",
+    date: "Jan 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+   {
+    title: "and more",
+    company_name: "EX: redux, C, C#, C++, HTML, three.js, etc.",
+    icon: redux,
+    iconBg: "#383E56",
+    date: "Jan 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "My Works",
+    description:
+      "Feel free to visit my LinkedIn profile, where you'll find a comprehensive overview of my professional background, skills, experiences, and the projects I'm passionate about. It’s the best place to learn more about who I am, what I do, and how I grow.",
+    tags: [
+      {
+        name: "LinkedIn",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Skills",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Works",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: linkedin_home,
+    source_code_link: "https://www.linkedin.com/in/heechan-yang-55aa8133b",
+  },
+  {
+    name: "My School",
+    description:
+      "Explore the official LinkedIn page of Yongin Samuel International School (YSIS) — a vibrant community dedicated to academic excellence, global citizenship, and nurturing future leaders. Stay connected with our latest updates, events, and inspiring stories from students, alumni, and educators.",
+    tags: [
+      {
+        name: "LinkedIn",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Information",
+        color: "green-text-gradient",
+      },
+      {
+        name: "YSIS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: School_LinkedIn,
+    source_code_link: "https://www.linkedin.com/company/samuel-international-school/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.                       ",
+    tags: [
+      {
+        name: "github",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "coding",
+        color: "green-text-gradient",
+      },
+      {
+        name: "projects",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://www.linkedin.com/in/heechan-yang-55aa8133b",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
